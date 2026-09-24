f8821a49f261c036c7a2d4b6338505677f5878bd
Aluno Fatec <aluno@fateclab.net>
2026-09-24 11:20:42 -0300

    feat: Create gitignore inside backend

--- backend/obj/Release/net9.0/backend.AssemblyInfo.cs
+++ backend/obj/Release/net9.0/backend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6c35befb34831fffce26f64180b4c49d4735c8a")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
